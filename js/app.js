abfa02e4f8c752254410b95be8733b5e275b5f5c
mimerkou <mimerkou@gmail.com>
2020-04-16 21:44:59 +0300

    added animation on delete button

--- js/app.js
+++ js/app.js
@@ -46,7 +46,10 @@ function deleteCheck(e) {
     // delete todo
     if (item.classList[0] === 'trash-btn') {
         const todo = item.parentElement;
-        todo.remove();
+        todo.classList.add('fall');
+        todo.addEventListener('transitionend', function() {
+            todo.remove();
+        });
     }
 
     // check mark
